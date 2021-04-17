b1e5955462443f5ff7ef39e070927c3cf0119206
Guuuuo <1460412+guuuuo@users.noreply.github.com>
2021-04-17 22:58:48 +0800

    Update redis config for qa environment

--- back-office-persistence/src/main/java/com/bn/config/RedisConfig.java
+++ back-office-persistence/src/main/java/com/bn/config/RedisConfig.java
@@ -1,20 +1,36 @@
 package com.bn.config;
 
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.util.StringUtils;
 
 /**
  * @author beckl
  */
 @Configuration
 public class RedisConfig {
+
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration());
+    public LettuceConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        if (StringUtils.hasText(redisProperties.getHost())) {
+            redisStandaloneConfiguration.setHostName(redisProperties.getHost());
+        }
+        if (redisProperties.getPort() != 0) {
+            redisStandaloneConfiguration.setPort(redisProperties.getPort());
+        }
+        if (redisProperties.getPort() != 0) {
+            redisStandaloneConfiguration.setDatabase(redisProperties.getDatabase());
+        }
+        if (StringUtils.hasText(redisProperties.getPassword())) {
+            redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
+        }
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     // @Bean
